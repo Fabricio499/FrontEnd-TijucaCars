65d88de460a320985074b7c18af7e862253af2e4
Dev-fabricio <devfabricio1@gmail.com>
2022-05-23 13:08:06 -0300

    0003

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'https://git.heroku.com/databasetijucacars.git/'
+    baseURL: 'https://databasetijucacars.herokuapp.com/'
 });
 
 Api.interceptors.request.use(async(config)=>{
